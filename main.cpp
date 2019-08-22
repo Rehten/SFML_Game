3e8e4008b4b916bfc71d8068d498b7b5bdbf32d4
Rehten <Mroow@yandex.ru>
2019-08-22 22:54:21 +0300

    game00_boilerplate

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::CircleShape shape(50.f);
+    shape.setFillColor(sf::Color::Cyan);
 
     while (window.isOpen())
     {
